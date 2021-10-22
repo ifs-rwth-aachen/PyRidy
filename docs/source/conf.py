b3680069291cb6900d2429f84be8d85932c19022
Philipp Leibner <philipp.leibner@gmail.com>
2021-10-22 11:31:42 +0200

    changed sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,15 @@ extensions = [
 	'sphinx.ext.duration',
 	'sphinx.ext.autodoc',
 	'sphinx.ext.autosummary',
-	'sphinx.ext.napoleon'
+	'sphinx.ext.napoleon',
+	'sphinx.ext.intersphinx',
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
